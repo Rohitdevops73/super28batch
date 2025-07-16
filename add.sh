42987b46af52dbe1bbcd7679aef84ad92fda80b2
Rohitdevops73 <rohitpatil.cse@gmail.com>
2025-07-16 06:00:12 +0000

    first

--- add.sh
+++ add.sh
@@ -4,16 +4,16 @@ if [ "$1" == "add" ]; then
 	commit_message=$2
 	
 	git status
-	echo "running command is git status"
+	echo "Rohit_comment:running command is git status"
 
 	git add .
-	echo "running git add"
+	echo "Rohit_comment:running git add"
 
 	git commit -m "$commit_message"
-	echo "git commit is running with commit mesage "
+	echo "Rohti_comment:git commit is running with commit mesage "
 
 	git push
-	echo " files are pushed to repo"
+	echo "Rohit_comment:files are pushed to repo"
 
 fi
 
